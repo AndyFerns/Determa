c621a35aec2608ae1c0896f55d71c18ce52f2c3f
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 02:48:41 +0530

    implemented token helper function definitions

---
+++ src/token.c
@@ -0,0 +1,73 @@
+/**
+ * @file token.c
+ * @author Andrew Fernandes
+ * @brief Implementation of helper functions for the Token struct.
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "token.h"
+#include <string.h>
+
+/**
+ * Creates a new token
+ */
+Token create_token(TokenType type, const char* lexeme, int length, int line) {
+    Token token;
+
+    token.type = type;
+    token.lexeme = lexeme;
+    token.length = length;
+    token.line = line;
+
+    return token;
+}
+
+/**
+ * @brief Prints a token to stdout in a human-readable format
+ * 
+ * @param token The token to print
+ */
+void print_token(Token token) {
+    printf("[Line %-3d] %-15s '%.*s'\n",
+           token.line,
+           token_type_to_string(token.type),
+           token.length,
+           token.lexeme);
+}
+
+
+/**
+ * Converts a TokenType enum to a string.
+ */
+const char* token_type_to_string(TokenType type) {
+    switch (type) {
+        // Single-Character tokens
+        case TOKEN_LPAREN:    return "TOKEN_LPAREN";
+        case TOKEN_RPAREN:    return "TOKEN_RPAREN";
+
+        // Arithmetic Operators and Assignment
+        case TOKEN_PLUS:      return "TOKEN_PLUS";
+        case TOKEN_MINUS:     return "TOKEN_MINUS";
+        case TOKEN_STAR:      return "TOKEN_STAR";
+        case TOKEN_SLASH:     return "TOKEN_SLASH";
+        case TOKEN_SEMICOLON: return "TOKEN_SEMICOLON";
+        case TOKEN_EQUALS:    return "TOKEN_EQUALS";
+
+        // Literals
+        case TOKEN_INT:       return "TOKEN_INT";
+        case TOKEN_ID:        return "TOKEN_ID";
+
+        // Keywords
+        case TOKEN_VAR:       return "TOKEN_VAR";
+        case TOKEN_PRINT:     return "TOKEN_PRINT";
+
+        // Control Tokens
+        case TOKEN_ERROR:     return "TOKEN_ERROR";
+        case TOKEN_EOF:       return "TOKEN_EOF";
+        default:              return "TOKEN_UNKNOWN";
+    }
+}
