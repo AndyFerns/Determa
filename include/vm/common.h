7606765a74132c7caa1fe08c264aa02d2d2bcca6
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 22:50:10 +0530

    central file for standard types used across the VM

---
+++ include/vm/common.h
@@ -0,0 +1,16 @@
+/**
+ * @file common.h
+ * @brief Common definitions and types for the Determa Virtual Machine.
+ */
+
+#ifndef VM_COMMON_H
+#define VM_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// Use this to toggle debug logging for the VM
+// #define DEBUG_TRACE_EXECUTION
+
+#endif // VM_COMMON_H
