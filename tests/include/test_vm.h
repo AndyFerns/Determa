d589c4cd52bc326d7c8d833a4fb50188ef89a709
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 23:13:43 +0530

    added test_vm headerfile for future reference

---
+++ tests/include/test_vm.h
@@ -0,0 +1,12 @@
+/**
+ * @file test_vm.h
+ * @brief Declares unit tests for the Virtual Machine backend.
+ */
+
+#ifndef TEST_VM_H
+#define TEST_VM_H
+
+// A simple sanity check to ensure the VM infrastructure links correctly
+void test_vm_initialization();
+
+#endif // TEST_VM_H
