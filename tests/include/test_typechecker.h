9bad2d0f230617cb4da61ce0633bc8630085fcd6
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 21:32:59 +0530

    added basic doxygen comments for file description

---
+++ tests/include/test_typechecker.h
@@ -0,0 +1,11 @@
+/**
+ * @file test_typechecker.h
+ * @author Andrew Fernandes
+ * @brief Testcase headerfile for Typechecking
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
---
+++ tests/typechecker/test_typechecker.c
@@ -0,0 +1,10 @@
+/**
+ * @file test_typechecker.c
+ * @author Andrew Fernandes
+ * @brief Testcases for typechecking
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
