3b7fc863fa469abdab2863b26200b7f66e4872a5
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 20:58:46 +0530

    added first half of symbol table function definitions

--- src/symbol.c
+++ src/symbol.c
@@ -8,3 +8,35 @@
  * @copyright Copyright (c) 2025
  * 
  */
+
+#include "types.h"
+#include "symbol.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void symbol_table_init(SymbolTable* table) {
+    table->capacity = 16;
+    table->count = 0;
+    table->current_depth = 0;
+    table->symbols = (Symbol*)malloc(sizeof(Symbol) * table->capacity);
+}
+
+void symbol_table_free(SymbolTable* table) {
+    free(table->symbols);
+    table->count = 0;
+}
+
+void symbol_table_enter_scope(SymbolTable* table) {
+    table->current_depth++;
+}
+
+void symbol_table_exit_scope(SymbolTable* table) {
+    // Remove all symbols that belong to the current depth
+    while (table->count > 0 && 
+           table->symbols[table->count - 1].depth == table->current_depth) {
+        table->count--;
+    }
+    table->current_depth--;
+}
