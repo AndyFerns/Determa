d222742a8a4e35a1844f93b27d86d196687646d6
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 20:48:01 +0530

    The symbol table will be implemented here

---
+++ src/symbol.c
@@ -0,0 +1,10 @@
+/**
+ * @file symbol.c
+ * @author Andrew Fernandes
+ * @brief Implementation of the Symbol Table (hash map or list)
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
