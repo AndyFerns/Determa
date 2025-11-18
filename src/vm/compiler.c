c0ed8e28c566ff05930208a1583c294e3e878ff7
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 23:10:53 +0530

    added basic stub for compiler.c with TODO for ast parsing and bytecode emitting

---
+++ src/vm/compiler.c
@@ -0,0 +1,19 @@
+/**
+ * @file compiler.c
+ * @author your name (you@domain.com)
+ * @brief A compiler stub used to Walk AST and emit bytecode
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "vm/compiler.h"
+#include <stdio.h>
+
+int compile_ast(struct AstNode* ast, Chunk* chunk) {
+    printf("Compiler: compile_ast called (Stub)\n");
+    // TODO: Walk AST and emit bytecode
+    return 1;
+}
