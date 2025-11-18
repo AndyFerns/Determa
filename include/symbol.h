e0dcaaf9a499713efdf88dd61c3a662e5e8e4669
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 20:47:29 +0530

    headerfile for defining the symbol table structure

---
+++ include/symbol.h
@@ -0,0 +1,10 @@
+/**
+ * @file symbol.h
+ * @author Andrew Fernandes
+ * @brief Defines the Symbol Table structure.
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
