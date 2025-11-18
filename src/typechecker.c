f3abecd3db4b11ee13a6b601c65c1ee6c7a1ec1b
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 20:48:35 +0530

    clubbing the AST walker logic + semantic analysis together under typechecker.c

---
+++ src/typechecker.c
@@ -0,0 +1,10 @@
+/**
+ * @file typechecker.c
+ * @author Andrew Fernandes
+ * @brief The recursive AST walker that validates logic.
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
