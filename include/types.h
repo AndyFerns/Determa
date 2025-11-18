1b4477ab2d21715865dec99be9e406f9bf3d313a
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 20:45:07 +0530

    headerfile for defining DataType enum and helpers

---
+++ include/types.h
@@ -0,0 +1,10 @@
+/**
+ * @file types.h
+ * @author Andrew Fernandes
+ * @brief Defines DataType enum and helpers.
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
