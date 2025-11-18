6f42dce3604663f9d1f08e1175b9f06572a482d6
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-18 20:47:48 +0530

    semantic analyzer will be clubbed under this broad headerfile

---
+++ include/typechecker.h
@@ -0,0 +1,10 @@
+/**
+ * @file typechecker.h
+ * @author Andrew Fernandes
+ * @brief Public API for the semantic analyzer. 
+ * @version 0.1
+ * @date 2025-11-18
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
