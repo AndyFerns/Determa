d324b969085b8b80b60b5c4191da94ec615dbb25
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-30 18:53:52 +0530

    created headerfile for gc testcase suite

---
+++ tests/include/test_gc.h
@@ -0,0 +1,16 @@
+/**
+ * @file test_gc.h
+ * @brief simple testcases to check the garbage collector's validity
+ * @version 0.1
+ * @date 2025-11-30
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef TEST_GC_H
+#define TEST_GC_H
+
+void test_gc_suite();
+
+#endif // TEST_GC_H
